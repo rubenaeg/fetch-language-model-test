fdf74c4f6bed2c063482970403a149d1f2290449
Ruben <email@aegerter.tech>
2021-05-26 16:02:42 +0200

    :art: Add spacer

--- hooks/fetchLanguageModel.hook.js
+++ hooks/fetchLanguageModel.hook.js
@@ -1,11 +1,11 @@
 const { writeFileSync } = require("fs");
 const { BaseApp } = require("jovo-framework");
-const { Task } = require("@jovotech/cli-core");
+const { Task, printUserInput, Log } = require("@jovotech/cli-core");
 const { AirtableCMS } = require("jovo-cms-airtable");
 
 async function fetchLanguageModel() {
   const task = new Task(
-    "Getting language model from Airtable CMS",
+    printUserInput("Getting language model from Airtable CMS"),
     async () => {
       const cms = new AirtableCMS({
         apiKey: process.env.AIRTABLE_API_KEY,
@@ -61,6 +61,7 @@ async function fetchLanguageModel() {
     }
   );
   await task.run();
+  Log.spacer();
 }
 
 module.exports = { fetchLanguageModel };
